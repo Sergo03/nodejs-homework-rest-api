5c1aec3c2e808621cd0dfca0038563690d8421a3
Sergey Karpynskyi <karpinskiy94@gmail.com>
2021-08-02 16:55:22 +0300

    hw5

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs/promises");
+const jimp=require('jimp')
+require("dotenv").config();
+
+
 
 
 const contactsRouter = require('./routes/api/contacts');
 const userRouter = require('./routes/api/user');
+const moment = require('moment');
 
 const app = express()
 
@@ -18,6 +26,59 @@ app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
 app.use('/api/users', userRouter);
+
+const uploadDir = `${__dirname}/public/avatars`;
+
+app.use('/avatars', express.static(uploadDir))
+
+const tempDir = path.join(process.cwd(), "tmp");
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb)=>{
+        cb(null, tempDir);
+    },
+    filename: (req, file, cb)=>{
+        cb(null, file.originalname);
+    },
+    limits: {
+        fileSize: 100000
+    }
+})
+const uploadMiddleware = multer({
+    storage
+});
+
+app.patch('/api/users/avatars', uploadMiddleware.single("avatar"), async(req, res, next)=> {
+  const { path: tempName, originalname } = req.file;
+  const resizeAvatar = jimp.read(tempName)
+    .then(img => {
+      img.resize(250, 250)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  const now = moment().format("YYYY-MM-DD_hh-mm-ss");
+
+  const fileName = `${now}_${originalname}`;
+  try {
+    const fullFileName = path.join(uploadDir, fileName);
+    await fs.rename(tempName, fullFileName)
+    const result = {
+      _id: req.body.name,
+      image:fileName
+    }
+    res.status(200).json({
+      
+    })
+  } catch (error) {
+    
+  }
+ 
+
+})
+
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
--- model/schema/user.js
+++ model/schema/user.js
@@ -19,7 +19,10 @@ const userSchema = Schema({
   token: {
     type: String,
     default: null,
-    }
+  },
+  avatarURL: {
+    type:String,
+  }
 
 })
 
